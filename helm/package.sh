0bcb1ab66c58ee7e44d430f010097ea1b03a6520
Matt S.Y. Ho <methodho@gmail.com>
2018-08-07 15:17:51 +0800

    break shell if one command failed

--- helm/package.sh
+++ helm/package.sh
@@ -12,19 +12,24 @@ for i in "${!args[@]}"; do
 	fi
 done
 
+if [[ -z "$chart" ]]; then
+  echo "chart is required";
+  exit 1;
+fi
+
 # Merge sit and client-a to values.yaml
-helm values $chart $values -o $chart
+helm values $chart $values -o $chart || exit 1;
 
 # Filter out files in templates-dir and copy to tmp folder
-helm filter $chart -o tmp
+helm filter $chart -o tmp || exit 1;
 
 # Pack chart archive
-helm package tmp/$chart
+helm package tmp/$chart || exit 1;
 
 # Remove tmp folder 
-rm -rf tmp 
+rm -rf tmp || exit 1;
 
 # Restore values.yaml from backup file
-mv $chart/values.yaml.bak $chart/values.yaml
+mv $chart/values.yaml.bak $chart/values.yaml || exit 1;
 
 exit 0
