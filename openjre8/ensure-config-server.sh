a7d4384460059320a2bc6e42fe44fbc8609f6b97
matt <methodho@gmail.com>
2019-09-05 15:33:56 +0800

    update

--- openjre8/ensure-config-server.sh
+++ openjre8/ensure-config-server.sh
@@ -3,14 +3,14 @@ retval=$?
 
 if test "$retval" != "0"; then
 	END=$(awk -v min=3 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-    echo "shutting down in $END secs..."
-    for i in $(seq $END);
-    do
-      sleep 1
-      # 由於 alpine 的 seq 只能從小到大,  所以這邊用減的
-      echo $(($END - $i))
-    done
-   exit $retval
+	echo "shutting down in $END secs..."
+	for i in $(seq $END);
+	do
+		sleep 1
+		# 由於 alpine 的 seq 只能從小到大, 所以這邊用減的
+		echo $(($END - $i))
+	done
+	exit $retval
 fi
 
 exit 0
