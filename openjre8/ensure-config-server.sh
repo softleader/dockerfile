19ab7c9d33de9960e7bbad11364a9f26e7b7018d
matt <methodho@gmail.com>
2019-09-05 15:30:55 +0800

    update

--- openjre8/ensure-config-server.sh
+++ openjre8/ensure-config-server.sh
@@ -1,1 +1,15 @@
-curl -L --fail http://softleader-config-server:8888/health -o /dev/null
+curl -Lf http://softleader-config-server:8888/health
+retval=$?
+
+if test "$retval" != "0"; then
+	END=$(awk -v min=3 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+    echo "shutting down in $END secs..."
+    for i in $(seq $END);
+    do
+      sleep 1
+      echo $(($END - $i))
+    done
+   exit $retval
+fi
+
+exit 0
