2a380a020142971a107a6212474aa4f19dde60f2
matt <methodho@gmail.com>
2020-10-06 15:58:04 +0800

    add taipei timezone for azul openjdk11

---
+++ azul/build-taipei.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile.taipei -t harbor.softleader.com.tw/thirdparty/zulu-openjdk-alpine:11-jre-taipei .
+docker push harbor.softleader.com.tw/thirdparty/zulu-openjdk-alpine:11-jre-taipei
