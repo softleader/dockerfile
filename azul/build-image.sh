e1d8c0bb7d877b33850951d6a5d160b831c77bae
Shihyu Ho <methodho@gmail.com>
2021-04-06 10:15:09 +0800

    update

--- azul/build-image.sh
+++ azul/build-image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 build_image() {
+  set -ex
   docker build -f Dockerfile.11-taipei -t harbor.softleader.com.tw/library/zulu-openjdk-alpine:$1 .
   docker push harbor.softleader.com.tw/library/zulu-openjdk-alpine:$1
+  set +ex
 }
 
 tags=(11-jre-taipei 11-font-jre-taipei 8-jre-taipei 8-fornt-jre-taipei)
