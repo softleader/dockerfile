127d950607150958619a8f775c22ac9ba1c8484f
Shihyu Ho <methodho@gmail.com>
2022-04-28 18:36:35 +0800

    feat: add centos java base image

---
+++ azul-zulu/centos/build-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+build_image() {
+  set -ex
+  docker build -f Dockerfile.$1 -t ${image}:$1 .
+  docker push ${image}:$1
+  set +ex
+}
+
+image=harbor.softleader.com.tw/library/zulu-openjdk-centos
+tags=(11-font-jre-taipei)
+
+if [ "$1" = "ls" ]; then
+  for tag in "${tags[@]}"
+  do
+      echo "$tag"
+  done
+elif [ "$1" ]; then
+  build_image $1
+else
+  for tag in "${tags[@]}"
+  do
+      build_image $tag
+  done
+fi
