98a96413b98614868572c63ae5f24002f122e8d4
Shihyu Ho <methodho@gmail.com>
2025-10-26 21:44:03 +0800

    feat: dynamic tags

--- azul-zulu/build-image-no-cache.sh
+++ azul-zulu/build-image-no-cache.sh
@@ -8,7 +8,7 @@ build_image() {
 }
 
 image=harbor.softleader.com.tw/library/zulu-openjdk-alpine
-tags=(17-jre-taipei 17-font-jre-taipei 11-jre-taipei 11-font-jre-taipei 8-jre-taipei 8-font-jre-taipei)
+tags=($(ls -1 Dockerfile.* | sed 's/Dockerfile.//' | sort -t- -k1 -n -r))
 
 if [ "$1" = "ls" ]; then
   for tag in "${tags[@]}"
